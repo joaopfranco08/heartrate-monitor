a970560374cbe12cf6eb2bec7fafb61d7cdc18f5
Barbara Tong <barbarachantong@gmail.com>
2023-07-12 22:26:48 -0300

    menor dc motor

---
+++ lab05/ligar_motor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <wiringPi.h>
+//#include <unistd.h>
+//#include <math.h>
+
+#define pino_PWM0 18
+
+int main(){
+        int dc = 8;
+        int ciclos;
+        
+        int range = 128;
+
+        printf("Iniciando... Usando divisor\n");
+
+        wiringPiSetupGpio();
+        pinMode(pino_PWM0, PWM_OUTPUT);
+
+        pwmSetMode(PWM_MODE_MS);
+        pwmSetRange(range);
+        pwmSetClock(574);
+
+	pwmWrite(pino_PWM0, dc);
+	usleep(300000);
+        pwmWrite(pino_PWM0, 0);
+
+        printf("Fim.\n");
+        return 0;
+}
