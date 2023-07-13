6c4104588404d6c3645ac1c73cd1ebab45cbcef7
Barbara Tong <barbarachantong@gmail.com>
2023-07-12 22:12:57 -0300

    add alarmes

---
+++ lab05/alarme_alta_prioridade.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <wiringPi.h>
+//#include <unistd.h>
+//#include <math.h>
+
+#define pino_PWM0 18
+
+int main(){
+	int dc = 64;
+	int ciclos;
+	float soundFrequency = 261.1; // Hz
+	int range = 128;
+
+	int divisor = ((19.2 * (1000000.0))/(soundFrequency*(float)range));
+
+	printf("Iniciando... Usando divisor = %d\n",divisor);
+
+	wiringPiSetupGpio();
+	pinMode(pino_PWM0, PWM_OUTPUT);
+
+	pwmSetMode(PWM_MODE_MS);
+	pwmSetRange(range);
+	pwmSetClock(divisor);
+	
+	int execucao;
+
+	for (execucao = 0; execucao < 2; execucao++) {
+
+		for(ciclos=0; ciclos < 3; ciclos++){
+			pwmWrite(pino_PWM0, dc);
+			usleep(300000);
+			pwmWrite(pino_PWM0, 0);
+			usleep(150000);
+		}
+		usleep(2000000);
+		for(ciclos=0; ciclos < 2; ciclos++){
+	                pwmWrite(pino_PWM0, dc);
+	                usleep(300000);
+	                pwmWrite(pino_PWM0, 0);
+	                usleep(150000);
+	        }
+		usleep(2000000);
+	}
+
+	printf("Fim.\n");
+	return 0;
+}
