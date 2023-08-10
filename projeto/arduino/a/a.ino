54e5bd8a899460ae5467b50064ae9d523345d1f0
Barbara Tong <barbarachantong@gmail.com>
2023-08-10 19:10:13 -0300

    atualizando leitor

--- projeto/arduino/a/a.ino
+++ projeto/arduino/a/a.ino
@@ -1,66 +1,43 @@
 unsigned long start = 0, finish = 0;
-float delta, bpm, deltaS, frequencia, cond, bpmMed, acum;
+float delta, bpm, deltaS, frequencia, cond,bpmMed, acum;
 int flag = 0;
 int SensorIn = 0, x = 1, i = 0;
 
-void setup() {
-// put your setup code here, to run once:
+void setup(){
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   SensorIn = analogRead(A0);
   Serial.println(SensorIn);
-
-  cond = SensorIn * x;
-
-  if (cond > 150 && flag == 0) {
+  
+  cond = SensorIn = x;
+  if(cond > 150 && flag ==0){
     start = millis();
     flag = 1;
-    //Serial.print("Time");
-    //Serial.println(start);
     x = 0.01;
   }
   
   SensorIn = analogRead(A0);
   
-  
-  if (SensorIn < 890) {
+  if(SensorIn < 890){
     x = 1;
   }
   
-  cond = SensorIn * x;
-
-  if (cond > 150 && flag == 1) {
+  cond = SensorIn*x;
+  
+  if (cond > 150 && flag == 1){
     finish = millis();
     flag = 0;
-    //Serial.print("Time2 ");
-    //Serial.println(finish);
-
     delta = float(finish - start);
-    frequencia = 1000/ delta;
+    frequencia = 1000/delta;
     
-    bpm = frequencia * 60;
-    if (bpm > 60) {
-      Serial.println(bpm);
-      acum = acum + bpm;
-      i++;
-      x = 0.01;
-      
+    bpm = frequencia *60;
+    if(bpm > 60){
+      Serial.print("Batimentos por minuto: ");
+      Serial.println(bpmMed);
+      acum = 0;
+      i = 0;
     }
-  
-  } 
-  if(i >= 5){
-    bpmMed = acum / 5;
-    Serial.print("Batimentos por minuto: ");
-    Serial.println(bpmMed);
-    acum = 0;
-    i = 0;
-  }
-
-
-  
+  }    
 }
-
-
