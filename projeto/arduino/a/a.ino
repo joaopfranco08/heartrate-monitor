373cdda7d128499632ea630055491261fa831155
Barbara Tong <barbarachantong@gmail.com>
2023-08-10 20:30:09 -0300

    LED e BUZZER adicionados

--- projeto/arduino/a/a.ino
+++ projeto/arduino/a/a.ino
@@ -1,7 +1,8 @@
 unsigned long start = 0, finish = 0;
-float delta, bpm, deltaS, frequencia, cond,bpmMed, acum;
+float delta, bpm, deltaS, frequencia, cond, bpmMed, acum;
 int flag = 0;
 int SensorIn = 0, x = 1, i = 0;
+int led_pin = 8, buzz_pin = 10, buzz_freq = 262, buzz_time = 400;
 
 void setup(){
   Serial.begin(9600);
@@ -9,9 +10,9 @@ void setup(){
 
 void loop() {
   SensorIn = analogRead(A0);
-  Serial.println(SensorIn);
   
-  cond = SensorIn = x;
+  cond = SensorIn * x;
+  
   if(cond > 150 && flag ==0){
     start = millis();
     flag = 1;
@@ -33,11 +34,22 @@ void loop() {
     frequencia = 1000/delta;
     
     bpm = frequencia *60;
+
     if(bpm > 60){
       Serial.print("Batimentos por minuto: ");
       Serial.println(bpmMed);
+      
+      if(bpm > 100){
+        tone(buzz_pin, buzz_freq, 200);
+      }
+      
       acum = 0;
       i = 0;
     }
-  }    
+    digitalWrite(led_pin, HIGH);
+  }
+  else{
+    digitalWrite(led_pin, LOW);
+  }
+
 }
